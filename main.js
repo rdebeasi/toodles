aeb31d8be995098aa6fd3cda62d90c19735f1768
Ryan DeBeasi <hi@ryandebeasi.com>
2016-10-26 15:04:12 -0400

    Create ability to add tasks

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 (function(){
-  console.log('sialenc iz goldan');
+  function Task(descr) {
+    this.done = false;
+    this.descr = descr;
+
+    return this;
+  }
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      tasks: [
+        new Task('Do a thing'),
+        new Task('Do another thing')
+      ],
+      newTodoDescr: ''
+    },
+    methods: {
+      addNew: function() {
+        this.tasks.push( new Task(this.newTodoDescr) );
+        this.newTodoDescr = '';
+      }
+    }
+  })
 })();
